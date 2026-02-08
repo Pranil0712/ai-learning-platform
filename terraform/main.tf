6e81206c21ee1a841073ba53e7e187669c7f8502
Pranil0712 <pranilpatel55@gmail.com>
2026-02-08 13:25:35 +0530

    main_tf_updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,35 @@
 resource "aws_security_group" "devops_sg" {
-    name = "devops_sg"
-    ingress {from_port=22 to_port=22 protocol="tcp" cidr_blocks=["0.0.0.0/0"]}
-    ingress {from_port=8080 to_port=80 protocol="tcp" cidr_blocks=["0.0.0.0/0"]}
-    ingress {from_port=30000 to_port=32767 protocol="tcp" cidr_blocks=["0.0.0.0/0"]}
-}   
+  name = "devops_sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "devops_ec2" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key_name
-    vpc_security_group_ids =  [aws_security_group.devops_sg.id]
-    tags = {Name = "ai-learning-devops"}
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "ai-learning-devops"
+  }
 }
