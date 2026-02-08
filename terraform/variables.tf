7ee4db47564a18d0161af17f4fba90ff26168729
Pranil0712 <pranilpatel55@gmail.com>
2026-02-08 13:18:10 +0530

    tf_file_change

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,14 @@
-variable "ami_id" {}
-variable "instance_type" {}
-variable "key_name"{}
+variable "ami_id" {
+  type    = string
+  default = "ami-03f487875bd4384ee"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.medium"
+}
+
+variable "key_name" {
+  type    = string
+  # Leave default out so you are forced to remember which key you downloaded
+}
